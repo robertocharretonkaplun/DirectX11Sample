a859d6fb5fb2edb78c162f611619d0eabd8c192e
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2023-03-03 09:41:42 -0600

    main - Fixing some error of model loading

--- Tutorial07/src/ModelLoader.cpp
+++ Tutorial07/src/ModelLoader.cpp
@@ -31,40 +31,42 @@ ModelLoader::Load(std::string objFileName) {
   LoadData LD;
   objl::Loader Loader;
 
-  Loader.LoadFile(objFileName);
+  bool Model = Loader.LoadFile(objFileName);
 
-  int totalVertices = Loader.LoadedVertices.size();
-  int loadedVertices = 0;
+  if (Model) {
+    int totalVertices = Loader.LoadedVertices.size();
+    int loadedVertices = 0;
 
-  LD.name = objFileName;
+    LD.name = objFileName;
 
-  WARNING("ModelLoader::Load() [Load of file: " << objFileName.c_str() << "] \n");
+    WARNING("ModelLoader::Load() [Load of file: " << objFileName.c_str() << "] \n");
 
-  // Load and set data to custom structure (SimpleVertex)
-  LD.vertex.resize(Loader.LoadedVertices.size());
+    // Load and set data to custom structure (SimpleVertex)
+    LD.vertex.resize(Loader.LoadedVertices.size());
 
-  for (int i = 0; i < LD.vertex.size(); i++) {
-    // Load Positions
-    LD.vertex[i].Pos.x = Loader.LoadedVertices[i].Position.X;
-    LD.vertex[i].Pos.y = Loader.LoadedVertices[i].Position.Y;
-    LD.vertex[i].Pos.z = Loader.LoadedVertices[i].Position.Z;
+    for (int i = 0; i < LD.vertex.size(); i++) {
+      // Load Positions
+      LD.vertex[i].Pos.x = Loader.LoadedVertices[i].Position.X;
+      LD.vertex[i].Pos.y = Loader.LoadedVertices[i].Position.Y;
+      LD.vertex[i].Pos.z = Loader.LoadedVertices[i].Position.Z;
 
-    // Load Textures
-    LD.vertex[i].Tex.x = Loader.LoadedVertices[i].TextureCoordinate.X;
-    LD.vertex[i].Tex.y = Loader.LoadedVertices[i].TextureCoordinate.Y;
-    loadedVertices++;
-    percentage = (float)loadedVertices / (float)totalVertices * 100;
-    WARNING("Cargando modelo: " << percentage << "% \n");
+      // Load Textures
+      LD.vertex[i].Tex.x = Loader.LoadedVertices[i].TextureCoordinate.X;
+      LD.vertex[i].Tex.y = Loader.LoadedVertices[i].TextureCoordinate.Y;
+      loadedVertices++;
+      percentage = (float)loadedVertices / (float)totalVertices * 100;
+      WARNING("Cargando modelo: " << percentage << "% \n");
     
-  }
-  // Load and set data to custom structure (Indices)
-  LD.index.resize(Loader.LoadedIndices.size());
-  for (int i = 0; i < LD.index.size(); i++) {
-    LD.index[i] = Loader.LoadedIndices[i];
-  }
+    }
+    // Load and set data to custom structure (Indices)
+    LD.index.resize(Loader.LoadedIndices.size());
+    for (int i = 0; i < LD.index.size(); i++) {
+      LD.index[i] = Loader.LoadedIndices[i];
+    }
 
-  LD.numVertex = LD.vertex.size();
-  LD.numIndex = LD.index.size();
+    LD.numVertex = LD.vertex.size();
+    LD.numIndex = LD.index.size();
+  }
   return LD;
 }
 
